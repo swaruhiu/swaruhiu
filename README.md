ba2af357c6e6f4fc89c8ed2c8c0e021991a32b8b
Waruhiu <warupoems@gmail.com>
2023-08-01 22:00:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center"> Hi 👋, I'm Steven Waruhiu!</a></h1>
+<h3 align="center">I'm an engineer on a mission to turn lines of code into magical creations. I thrive on challenges and love bringing ideas to life through the art of coding. Let's build something extraordinary together!</h3>
+
+<br />
+<br />
+
+- 🔭 &ensp;I’m currently working on building Apps!
+- 🌱 &ensp;I’m currently learning Flutter and React Native ❤️
+- 👯 &ensp;I’m looking to collaborate with other content creators.
+- ⚡ &ensp;Fun fact: I love football ⚽️ & chess ♜
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Steven Waruhiu</h1>
+        <p>Software Engineer</p>
+      </header>
+      <section id="about">
+        <h3>
+          I'm an engineer on a mission to turn lines of code into magical
+          creations. I thrive on challenges and love bringing ideas to life
+          through the art of coding. Let's build something extraordinary
+          together!
+        </h3>
+      </section>
+      <section id="experience">
+        <ul>
+          <li>
+            <p>🔭 &ensp;I’m currently working on building Apps!</p>
+            <p>🌱 &ensp;I’m currently learning Flutter and React Native ❤️</p>
+            <p>
+              👯 &ensp;I’m looking to collaborate with other content creators.
+            </p>
+            <p>⚡ &ensp;Fun fact: I love football ⚽️ & chess ♜</p>
+          </li>
+        </ul>
+      </section>
+      <footer>
+        <p>Contact me at: warupoems@gmail.com</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
